bc2e1263d698588eb80e0828200b9a8ff10bf189
Hari <hariprasadlucky20@gmail.com>
2025-09-18 12:43:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const[story,setStory]=useState();
+  const[city,setCity]=useState();
+  const apiurl="https://weatherappbackend4.onrender.com";
+
+  async function FetchStory(){
+    try{
+    const response=await fetch(`${apiurl}/api/weather/${city}`);
+    if(!response.ok){
+      return new Error("Not found")
+    }
+    const data =await response.text();
+    setStory(data);
+  }
+  catch(e){
+    setStory("Error :"+e.message())
+  }
+
+
+  
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="app">
+      <div className="weather-container">
+        <h1 className="title">🌦️ Weather Story App</h1>
+
+        <div className="input-box">
+          <input
+            type="text"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            placeholder="Enter your city..."
+          />
+          <button onClick={FetchStory}>Get Story</button>
+        </div>
+
+        {story && (
+          <div className="story-card">
+            <h2>Today's Weather Story</h2>
+            <p>{story}</p>
+          </div>
+        )}
+      </div>
     </div>
+          
   );
+
 }
 
 export default App;
